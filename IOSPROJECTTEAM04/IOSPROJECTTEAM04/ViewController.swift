1e850558e00898ee9b4bea7f2282a72bc4ec19a7
KaranamGopichand <srigopichand2000@gmail.com>
2024-02-22 22:05:02 -0600

    karanam Gopichand: First commit UI VIEW

---
+++ IOSPROJECTTEAM04/IOSPROJECTTEAM04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSPROJECTTEAM04
+//
+//  Created by Karanam Gopichand on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
